f1557deac5b20c17d46bd81862f6da4d50e4cf22
OpenThread <openfibers@gmail.com>
2016-05-30 11:11:37 +0800

    Initial Commit

---
+++ OTDelayLoadHelperDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OTDelayLoadHelperDemo
+//
+//  Created by openthread on 5/30/16.
+//  Copyright © 2016 openthread. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
